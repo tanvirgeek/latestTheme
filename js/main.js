e33e44e3109fd4c5e5a439865908342242854d40
Md Tanvir Alam <tanvirgeek@gmail.com>
2013-06-02 01:40:57 +0600

    got some control

--- js/main.js
+++ js/main.js
@@ -15,10 +15,28 @@ $(document).on('pop-initialized', function(){
 
     $('#four-column .asset-type-imagegroup.asset-size-4').hover(
         function(){
-            $(this).find('.frame').css({border:'1px solid #06d5ba'});
+            $(this).find('img').css("border","1px solid #27CCC0");
+            $(this).find('img').animate({
+                
+                width:'-=14',
+                height:'-=14',
+                padding: 7
+                
+                }, 300, function() {
+             // Animation complete.
+            });
         },
         function(){
-            $(this).find('.frame').css({border:'1px solid transparent'});
+            
+            $(this).find('img').animate({
+                width:'+=14',
+                height:'+=14',
+                padding: 0
+                
+                }, 300, function() {
+                     $(this).css("border","0px solid #27CCC0");
+            });;
         });
+    
 
 });
