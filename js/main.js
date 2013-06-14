45994d23e6bda7743f2900a0f3a4e01460b9e040
Md Tanvir Alam <tanvirgeek@gmail.com>
2013-06-15 01:04:27 +0600

    imageFrame Fixed

--- js/main.js
+++ js/main.js
@@ -1,51 +1,8 @@
-// var fixColumnHeights = function() {
-//   $('.columnizer-row').each(function(){
-//     var maxHeight = 0;
-//     $(this).find('.asset-inner').each(function(){
-//       $(this).height('auto');
-//       var innerHeight = $(this).innerHeight();
-//       if (innerHeight > maxHeight) {
-//         maxHeight = innerHeight;
-//       }
-//     });
-//     $(this).find('.asset-inner').innerHeight(maxHeight);
-//   });
-// }
+
 $(document).on('pop-initialized', function(){
 
-    /*$('#four-column .asset-type-imagegroup.asset-size-4').hover(
-        function(){
-            $(this).find('img').css("border","1px solid #27CCC0");
-            $(this).find('img').animate({
-                
-                width:'-=14',
-                height:'-=14',
-                padding: 7
-                
-                }, 300, function() {
-             // Animation complete.
-            });
-        },
-        function(){
-            
-            $(this).find('img').animate({
-                width:'+=14',
-                height:'+=14',
-                padding: 0
-                
-                }, 300, function() {
-                     $(this).css("border","0px solid #27CCC0");
-            });;
-        });
-        */
-        $('#header-region .slide').css({
-            width:'100%',
-            height: '100%'
-        });
-        $('#header-region .slide img').css({
-            width:'100%',
-            height: '100%'
-        });
+    
+       
     init();
 
 
@@ -57,9 +14,14 @@ $(document).on('pop-initialized', function(){
 function init(){
     
     var initialHeight=$('#pop-title').height();
+    if(initialHeight!==undefined){
+        alert('here');
+        $('#pop-title').css('height','30px');
+        $('#pop-title').append('<h3>Title</h3>');
+    
+  
     var fontSize=$('#pop-title').css('font-size');
     var lessFont=(parseInt(fontSize)-10).toString()+'px';
-    var initialHeightString = initialHeight.toString();
     var lessHeight = initialHeight-20;
     var lessHeightString = lessHeight.toString();
     var titleParentHeight=$('#titleParent').outerHeight(true);
@@ -87,7 +49,7 @@ function init(){
         'data-300':"bottom:-50px;",
         'data-400':"bottom:50px;"
     });
-    
+    }
     var s = skrollr.init();
     
     $('.goUp').click(function(){
